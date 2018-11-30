8b4adf56d2693a24665f062c799ae405163c340f
Utkarsh Shigihalli <onlyutkarsh@users.noreply.github.com>
2018-11-30 19:45:34 +0000

    delete obsolete files

--- VSOStatusInspector.Package/Entities/VSTSStatusResponse.cs
+++
@@ -1,9 +0,0 @@
-namespace VSTSStatusMonitor.Entities
-{
-    public class VSTSStatusResponse
-    {
-        public string Status { get; set; }
-        public string Message { get; set; }
-        public string Title { get; set; }
-    }
-}
